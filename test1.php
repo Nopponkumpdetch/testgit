12c901a73ecaa481d9db2590fa5a5d4ab4aa7962
BOMBGIT <noppon.kumpdetch@gmail.com>
2017-07-10 17:28:44 +0700

    add page by bomb

---
+++ test1.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="testcss.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<p>hello Bomb111</p>
+</body>
+</html>
